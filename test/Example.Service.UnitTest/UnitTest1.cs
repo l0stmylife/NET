b87433e434e3716c2c645fa201f91210c63d1cde
L0stmylife <jornpeeters.peeters@gmail.com>
2020-11-19 20:38:07 +0100

    copied

--- test/Example.Service.UnitTest/UnitTest1.cs
+++ test/Example.Service.UnitTest/UnitTest1.cs
@@ -1,19 +1,21 @@
 using Example.Service.Services;
 using NUnit.Framework;
 
-
-
 namespace Example.Service.UnitTest
 {
     public class Tests
     {
+        [SetUp]
+        public void Setup()
+        {
+        }
 
         [Test]
         public void ShouldReturnAListOfStaticValues()
         {
-        var unitUnderTest = new ValuesService();
-	    var values = unitUnderTest.GetValues();
-	    Assert.That(values, Is.EqualTo(new[] {"Value1", "Value2", "Value3"}));
+            var unitUnderTest = new ValuesServices();
+            var values = unitUnderTest.GetValues();
+            Assert.That(values, Is.EqualTo(new[] {"Value1", "Value2", "Value3"}));
         }
     }
 }
